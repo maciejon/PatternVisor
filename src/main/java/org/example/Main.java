8c0f539408305d03cb0d710cef882be526b2dcb1
Maciej Olpiński <132942969+maciejon@users.noreply.github.com>
2026-03-30 20:58:21 +0200

    najprostsze sprawdzanie singletona
    działa

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,95 @@
+package org.example;
+import com.github.javaparser.StaticJavaParser;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.body.*;
+
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        // najprostszy kod do testu
+        String code = "public class singleton { \n" +
+                "    private singleton() {} \n" +
+//                "   public singleton() {}\n" +
+                "   private static int instancja;" +
+                "   public static String getInstance(){}" +
+                "}";
+
+        CompilationUnit cu = StaticJavaParser.parse(code);
+
+        cu.findAll(ClassOrInterfaceDeclaration.class).forEach(analyzedClass -> {
+            System.out.println("Analizuję klasę: " + analyzedClass.getNameAsString());
+
+            // --------------------------- SZUKANIE SINGLETONA ---------------------------
+
+            // ------------------- WARUNEK NR.1 - prywatny konstruktor -------------------
+            boolean warunek_1 = false;
+            List<ConstructorDeclaration> constructors = analyzedClass.findAll(ConstructorDeclaration.class);
+
+            if (constructors.isEmpty() ){
+                System.out.println("Nie ma konstruktora, to nie singleton.");
+                warunek_1 = false;
+            }
+            for (ConstructorDeclaration constructor : constructors){
+                if (!constructor.isPrivate()){
+                    System.out.println("Nieprywatny konstruktor! To nie singleton.");
+                    warunek_1 = false;
+                    break;
+                }
+                else{
+                    System.out.println("Jest prywatny konstruktor. To może być singleton.");
+                    warunek_1 = true;
+                }
+            }
+            if(warunek_1){
+                System.out.println("Warunek nr.1 spełniony.");
+            }
+
+            else{
+                System.out.println("Warunek nr.1 nie spełniony.");
+            }
+
+            // ------------------- WARUNEK NR.2 - przechowuje Instance typu siebie -------------------
+            boolean warunek_2 = false;
+            for (FieldDeclaration field : analyzedClass.getFields()){
+                if (field.isStatic() && field.isPrivate()){
+                    for (VariableDeclarator variable : field.getVariables()){ // w jednym polu może być kilka zmiennych np. int x, y;
+                        if(variable.getTypeAsString().equals(analyzedClass.getNameAsString())){
+                            warunek_2 = true;
+                            System.out.println("Znaleziono prywatną statyczną zmienną o typie takim samym jak nazwa klasy.");
+                        }
+                    }
+                }
+            }
+            if(warunek_2){
+                System.out.println("Warunek nr.2 spełniony.");
+            }
+            else{
+                System.out.println("Warunek nr.2 nie spełniony.");
+            }
+
+            // ------------------- WARUNEK NR.3 - metoda statyczna zwracająca instancję -------------------
+            boolean warunek_3 = false;
+            for (MethodDeclaration method : analyzedClass.getMethods()){
+                if (method.isPublic() && method.isStatic() && method.getTypeAsString().equals(analyzedClass.getNameAsString())){
+                    warunek_3 = true;
+                    // dołożyć sprawdzanie co się dzieje w środku metody?
+                    System.out.println("Znaleziono statyczną publiczną metodę, która zwraca typ taki jak jest klasa.");
+                }
+            }
+            if(warunek_3){
+                System.out.println("Warunek nr.3 spełniony.");
+            }
+            else{
+                System.out.println("Warunek nr.3 nie spełniony.");
+            }
+
+            if(warunek_1 && warunek_2 && warunek_3){
+                System.out.println("Mamy singletona!");
+            }
+            else {
+                System.out.println("Nie znaleziono singletona!");
+            }
+        });
+    }
+}
